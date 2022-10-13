26008f21302635e90c139ee8596c8f5303aea3ed
Lien Cao <lienctm@gmail.com>
2022-10-13 09:39:53 +0700

    Modified files in alien_invasion project

--- hmw-Python Crash Course,2019/chapter12-13-14-project1/alien_invasion/settings.py
+++ hmw-Python Crash Course,2019/chapter12-13-14-project1/alien_invasion/settings.py
@@ -4,5 +4,6 @@ class Settings:
         """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 700
         self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
