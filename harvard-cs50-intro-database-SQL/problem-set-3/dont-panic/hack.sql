7022cceab0cd15aad372b996fac4fc91e871d9ca
Lien Cao <lienctm@gmail.com>
2026-05-29 15:39:13 -0700

    first commit

---
+++ harvard-cs50-intro-database-SQL/problem-set-3/dont-panic/hack.sql
@@ -0,0 +1,11 @@
+-- * pwd hashed: h('opps!') = 982c0381c279d139fd221fce974916e7 * --
+INSERT INTO "user_logs"("type", "old_username", "new_username", "old_password", "new_password")
+VALUES('update','admin', 'admin', 'e10adc3949ba59abbe56e057f20f883e', '982c0381c279d139fd221fce974916e7');
+
+UPDATE "users"
+SET "password" = '982c0381c279d139fd221fce974916e7'
+WHERE "username" = 'admin';
+
+DELETE FROM "user_logs"
+WHERE "new_username" = 'admin';
+
